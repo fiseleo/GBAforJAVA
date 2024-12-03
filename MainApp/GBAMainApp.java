3eb6ecd003234413f76ad67a5ffafeec7528e945
fiseleo <aaaa50306@gmail.com>
2024-12-03 14:00:31 +0800

    test

--- MainApp/GBAMainApp.java
+++ MainApp/GBAMainApp.java
@@ -9,37 +9,37 @@ import javafx.stage.Stage;
 public class GBAMainApp extends Application {
     @Override
     public void start(Stage primaryStage) {
-        // 设置窗口标题
-        primaryStage.setTitle("GBA 模拟器");
+        // Set window title
+        primaryStage.setTitle("GBA Emulator");
 
-        // 创建菜单栏
+        // Create menu bar
         MenuBar menuBar = new MenuBar();
-        Menu fileMenu = new Menu("文件");
-        MenuItem openItem = new MenuItem("打开 ROM");
-        MenuItem exitItem = new MenuItem("退出");
+        Menu fileMenu = new Menu("File");
+        MenuItem openItem = new MenuItem("Open ROM");
+        MenuItem exitItem = new MenuItem("Exit");
         fileMenu.getItems().addAll(openItem, exitItem);
         menuBar.getMenus().add(fileMenu);
 
-        // 创建主布局
+        // Create main layout
         VBox layout = new VBox();
         layout.getChildren().add(menuBar);
 
-        // 添加其他控件
-        Label label = new Label("欢迎使用 GBA 模拟器！");
-        Button startButton = new Button("开始游戏");
+        // Add other controls
+        Label label = new Label("Welcome to GBA Emulator!");
+        Button startButton = new Button("Start Game");
         layout.getChildren().addAll(label, startButton);
 
-        // 设置事件监听
+        // Set event listeners
         startButton.setOnAction(e -> {
             Alert alert = new Alert(Alert.AlertType.INFORMATION);
-            alert.setTitle("提示");
+            alert.setTitle("Notice");
             alert.setHeaderText(null);
-            alert.setContentText("游戏开始！");
+            alert.setContentText("Game started!");
             alert.showAndWait();
         });
         exitItem.setOnAction(e -> System.exit(0));
 
-        // 创建场景并显示
+        // Create scene and show
         Scene scene = new Scene(layout, 800, 600);
         primaryStage.setScene(scene);
         primaryStage.show();
